839cf2624d0711e5ab1821f070f27ba79a8115d1
Milo751 <gcamilo751@hotmail.com>
2024-10-04 11:41:18 -0500

    Logging

--- utils/logger.py
+++ utils/logger.py
@@ -1,11 +1,32 @@
+import os
 import logging
 
 from logging.handlers import RotatingFileHandler
 
 class Logger:
-    def setup_logging(log_level):
-        handler = RotatingFileHandler('logs/app.log', maxBytes=5000000, backupCount=2)
-        logging.basicConfig(handlers=[handler], 
-                            level=log_level,
-                            format='%(asctime)s - %(levelname)s - %(message)s'
-                            )
+    def __init__(self, log_folder='logs', log_file='app.log'):
+        if not os.path.exists(log_folder):
+            os.makedirs(log_folder)
+
+        log_path = os.path.join(log_folder, log_file)
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            handlers=[logging.FileHandler(log_path)]
+        )
+        self.logger = logging.getLogger()
+
+    def log_debug(self, message):
+        self.logger.debug(message)
+
+    def log_info(self, message):
+        self.logger.info(message)
+
+    def log_warning(self, message):
+        self.logger.warning(message)
+
+    def log_error(self, message):
+        self.logger.error(message)
+
+    def log_critical(self, message):
+        self.logger.critical(message)
